4878143b726d91e46a79d0932f70f17cd0b06971
Oscar Franco <ospfranco@gmail.com>
2024-09-26 11:52:14 -0400

    Fix not initializing java vm

--- android/src/main/java/com/opacitylabs/opacitycore/OpacityCore.kt
+++ android/src/main/java/com/opacitylabs/opacitycore/OpacityCore.kt
@@ -9,58 +9,57 @@ import org.mozilla.geckoview.GeckoRuntime
 
 object OpacityCore {
 
-    private lateinit var appContext: Context
-    private lateinit var cryptoManager: CryptoManager
-    private lateinit var _url: String
-    private var headers: Bundle = Bundle()
-    private lateinit var sRuntime: GeckoRuntime
+  private lateinit var appContext: Context
+  private lateinit var cryptoManager: CryptoManager
+  private lateinit var _url: String
+  private var headers: Bundle = Bundle()
+  private lateinit var sRuntime: GeckoRuntime
 
-    fun initialize(context: Context) {
-        appContext = context
-        sRuntime = GeckoRuntime.create(context.applicationContext)
-        cryptoManager = CryptoManager(appContext.applicationContext)
+  fun initialize(context: Context): Int {
+    appContext = context
+    sRuntime = GeckoRuntime.create(context.applicationContext)
+    cryptoManager = CryptoManager(appContext.applicationContext)
+    return init()
+  }
+
+  fun getRuntime(): GeckoRuntime {
+    if (!::sRuntime.isInitialized) {
+      throw IllegalStateException("GeckoRuntime is not initialized. Call initialize() first.")
     }
+    return sRuntime
+  }
 
-    fun getRuntime(): GeckoRuntime {
-        if (!::sRuntime.isInitialized) {
-            throw IllegalStateException("GeckoRuntime is not initialized. Call initialize() first.")
-        }
-        return sRuntime
-    }
+  fun securelySet(key: String, value: String) {
+    cryptoManager.set(key, value)
+  }
 
-    fun securelySet(key: String, value: String) {
-        cryptoManager.set(key, value)
-    }
+  fun securelyGet(key: String): String? {
+    return cryptoManager.get(key)
+  }
 
-    fun securelyGet(key: String): String? {
-        return cryptoManager.get(key)
-    }
+  fun prepareInAppBrowser(url: String) {
+    headers = Bundle()
+    _url = url
+  }
 
-    fun prepareInAppBrowser(url: String) {
-        headers = Bundle()
-        _url = url
-    }
+  fun setBrowserHeader(key: String, value: String) {
+    headers.putString(key, value)
+  }
 
-    fun setBrowserHeader(key: String, value: String) {
-        headers.putString(key, value)
-    }
+  fun presentBrowser() {
+    val intent = Intent()
+    intent.setClassName(appContext.packageName, "com.opacitylabs.opacitycore.InAppBrowserActivity")
+    intent.putExtra("url", _url)
+    intent.putExtra("headers", headers)
+    appContext.startActivity(intent)
+  }
 
-    fun presentBrowser() {
-        val intent = Intent()
-        intent.setClassName(
-                appContext.packageName,
-                "com.opacitylabs.opacitycore.InAppBrowserActivity"
-        )
-        intent.putExtra("url", _url)
-        intent.putExtra("headers", headers)
-        appContext.startActivity(intent)
-    }
+  fun closeBrowser() {
+    val closeIntent = Intent("com.opacitylabs.opacitycore.CLOSE_BROWSER")
+    LocalBroadcastManager.getInstance(appContext).sendBroadcast(closeIntent)
+    Log.d("OpacityCore", "Intent dispatched")
+  }
 
-    fun closeBrowser() {
-        val closeIntent = Intent("com.opacitylabs.opacitycore.CLOSE_BROWSER")
-        LocalBroadcastManager.getInstance(appContext).sendBroadcast(closeIntent)
-        Log.d("OpacityCore", "Intent dispatched")
-    }
-
-    external fun emitWebviewEvent(json: String)
+  private external fun init(): Int
+  external fun emitWebviewEvent(json: String)
 }
