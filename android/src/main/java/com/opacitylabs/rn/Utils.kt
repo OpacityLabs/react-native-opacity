955e890db9f26bf23363659436590df7b81f2f72
Oscar Franco <ospfranco@gmail.com>
2025-05-28 19:21:34 +0200

    Fix mapping

--- android/src/main/java/com/opacitylabs/rn/Utils.kt
+++ android/src/main/java/com/opacitylabs/rn/Utils.kt
@@ -1,10 +1,10 @@
 package com.opacitylabs.rn
 
 import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableMap
 
-fun mapToWritableMap(map: Map<String, Any>): WritableMap {
+fun mapToWritableMap(map: Map<String, Any?>): WritableMap {
     val writableMap = Arguments.createMap()
     for ((key, value) in map) {
         when (value) {
@@ -12,8 +12,9 @@ fun mapToWritableMap(map: Map<String, Any>): WritableMap {
             is Int -> writableMap.putInt(key, value)
             is Double -> writableMap.putDouble(key, value)
             is Boolean -> writableMap.putBoolean(key, value)
-            is Map<*, *> -> writableMap.putMap(key, mapToWritableMap(value as Map<String, Any>))
+            is Map<*, *> -> writableMap.putMap(key, mapToWritableMap(value as Map<String, Any?>))
             is List<*> -> writableMap.putArray(key, listToWritableArray(value))
+            null -> writableMap.putNull(key)
             else -> throw IllegalArgumentException("Unsupported type for key: $key value: $value")
         }
     }
@@ -28,8 +29,9 @@ fun listToWritableArray(list: List<*>): WritableArray {
             is Int -> writableArray.pushInt(item)
             is Double -> writableArray.pushDouble(item)
             is Boolean -> writableArray.pushBoolean(item)
-            is Map<*, *> -> writableArray.pushMap(mapToWritableMap(item as Map<String, Any>))
+            is Map<*, *> -> writableArray.pushMap(mapToWritableMap(item as Map<String, Any?>))
             is List<*> -> writableArray.pushArray(listToWritableArray(item))
+            null -> writableArray.pushNull()
             else -> throw IllegalArgumentException("Unsupported list item type")
         }
     }
