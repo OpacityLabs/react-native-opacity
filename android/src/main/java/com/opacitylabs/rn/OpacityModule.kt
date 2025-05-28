4e4a35b402eeca10736525b7433fbcdb79899517
Oscar Franco <ospfranco@gmail.com>
2025-05-28 19:47:33 +0200

    Fix wrong native mapping

--- android/src/main/java/com/opacitylabs/rn/OpacityModule.kt
+++ android/src/main/java/com/opacitylabs/rn/OpacityModule.kt
@@ -5,16 +5,15 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.module.annotations.ReactModule
+import com.opacitylabs.opacitycore.OpacityCore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import com.opacitylabs.opacitycore.OpacityCore
 
 @ReactModule(name = OpacityModule.NAME)
 class OpacityModule(private val reactContext: ReactApplicationContext) :
-  NativeOpacitySpec(reactContext),
-  LifecycleEventListener {
+        NativeOpacitySpec(reactContext), LifecycleEventListener {
 
   override fun getName(): String {
     return NAME
@@ -25,22 +24,24 @@ class OpacityModule(private val reactContext: ReactApplicationContext) :
   }
 
   override fun init(
-    apiKey: String,
-    dryRun: Boolean,
-    environment: Double,
-    shouldShowErrorsInWebView: Boolean,
-    promise: Promise
+          apiKey: String,
+          dryRun: Boolean,
+          environment: Double,
+          shouldShowErrorsInWebView: Boolean,
+          promise: Promise
   ) {
-    val environmentEnum = when (environment) {
-      0.0 -> OpacityCore.Environment.TEST
-      1.0 -> OpacityCore.Environment.LOCAL
-      2.0 -> OpacityCore.Environment.STAGING
-      3.0 -> OpacityCore.Environment.PRODUCTION
-      else -> {
-        promise.reject("Invalid Arguments", "Invalid environment value")
-        return
-      }
-    }
+    val environmentEnum =
+            when (environment) {
+              0.0 -> OpacityCore.Environment.TEST
+              1.0 -> OpacityCore.Environment.LOCAL
+              2.0 -> OpacityCore.Environment.STAGING
+              3.0 -> OpacityCore.Environment.STAGING
+              4.0 -> OpacityCore.Environment.PRODUCTION
+              else -> {
+                promise.reject("Invalid Arguments", "Invalid environment value")
+                return
+              }
+            }
     try {
       OpacityCore.initialize(apiKey, dryRun, environmentEnum, shouldShowErrorsInWebView)
       promise.resolve(null)
@@ -66,11 +67,11 @@ class OpacityModule(private val reactContext: ReactApplicationContext) :
   }
 
   override fun onHostDestroy() {
-//    TODO("Not yet implemented")
+    //    TODO("Not yet implemented")
   }
 
   override fun onHostPause() {
-//    TODO("Not yet implemented")
+    //    TODO("Not yet implemented")
   }
 
   override fun onHostResume() {
--- ios/OpacityModule.mm
+++ ios/OpacityModule.mm
@@ -10,19 +10,19 @@ OpacityEnvironment opacityEnvironmentFromDouble(double value) {
   } else if (value == 1.0) {
     return Local;
   } else if (value == 2.0) {
+    return Sandbox;
+  } else if (value == 3.0) {
     return Staging;
   } else {
     return Production;
   }
 }
 
-RCT_EXPORT_METHOD(init
-                  : (NSString *)apiKey dryRun
-                  : (BOOL)dryRun environment
-                  : (double)environment shouldShowErrorsInWebView
-                  : (BOOL)shouldShowErrorsInWebView resolve
-                  : (RCTPromiseResolveBlock)resolve reject
-                  : (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(
+    init : (NSString *)apiKey dryRun : (BOOL)dryRun environment : (double)
+        environment shouldShowErrorsInWebView : (BOOL)
+            shouldShowErrorsInWebView resolve : (RCTPromiseResolveBlock)
+                resolve reject : (RCTPromiseRejectBlock)reject) {
 
   OpacityEnvironment env = opacityEnvironmentFromDouble(environment);
   NSError *error;
@@ -31,20 +31,19 @@ RCT_EXPORT_METHOD(init
                                     andDryRun:dryRun
                                andEnvironment:env
                  andShouldShowErrorsInWebview:shouldShowErrorsInWebView
-                                    andError:&error];
+                                     andError:&error];
 
   if (status == 0) {
     resolve(nil);
   } else {
-    reject([NSString stringWithFormat:@"%d", status], [error localizedDescription], NULL);
+    reject([NSString stringWithFormat:@"%d", status],
+           [error localizedDescription], NULL);
   }
 }
 
-RCT_EXPORT_METHOD(getInternal
-                  : (NSString *)name params
-                  : (NSDictionary *)params resolve
-                  : (RCTPromiseResolveBlock)resolve reject
-                  : (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(getInternal : (NSString *)name params : (NSDictionary *)
+                      params resolve : (RCTPromiseResolveBlock)
+                          resolve reject : (RCTPromiseRejectBlock)reject) {
   [OpacityObjCWrapper get:name
                 andParams:params
                completion:^(NSDictionary *res, NSError *error) {
