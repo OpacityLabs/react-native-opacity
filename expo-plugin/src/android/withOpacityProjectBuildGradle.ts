ffe5371686a00d836817ec7d15f080e329e81981
Oscar Franco <ospfranco@gmail.com>
2025-12-16 08:54:39 -0400

    Revert

--- expo-plugin/src/android/withOpacityProjectBuildGradle.ts
+++ expo-plugin/src/android/withOpacityProjectBuildGradle.ts
@@ -5,27 +5,14 @@ import {
 
 export const withOpacityProjectBuildGradle: ConfigPlugin = (config) => {
   return withProjectBuildGradle(config, async (config) => {
-    let { contents } = config.modResults;
-
-    // Add Mozilla maven repository if not present
-    if (!contents.match('maven.mozilla.org')) {
-      contents = contents.replace(
+    if (!config.modResults.contents.match('maven.mozilla.org')) {
+      config.modResults.contents = config.modResults.contents.replace(
         /mavenCentral\(\)/g,
-        `mavenCentral()
+        `
+        mavenCentral()
         maven { url "https://maven.mozilla.org/maven2/" }`
       );
     }
-
-    // Add jitpack repository if not present
-    if (!contents.match('jitpack.io')) {
-      contents = contents.replace(
-        /mavenCentral\(\)/g,
-        `mavenCentral()
-        maven { url 'https://jitpack.io' }`
-      );
-    }
-
-    config.modResults.contents = contents;
     return config;
   });
 };
